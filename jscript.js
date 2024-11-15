fe518ff6f202a1f370906a987acece8800869512
marcelarlaura <171378249+marcelarlaura@users.noreply.github.com>
2024-11-15 17:07:30 -0500

    add functionality to decimal button so decimal numbers can be entered by user. Also round operations to maximum two decimals so as to not oversaturate display screen with decimal numbers

--- jscript.js
+++ jscript.js
@@ -4,7 +4,12 @@ function add(num1,num2){
     } else if (num1 == undefined || num1 == NaN){
         num1 = 0;
     } 
-    return num1+num2;
+    if (Number.isInteger(num1 + num2)){
+        return num1+num2;
+    } else {
+        return Number((num1+num2).toFixed(2));
+    }
+    
 }
 
 function subtract(num1,num2){
@@ -13,7 +18,12 @@ function subtract(num1,num2){
     } else if (num1 == undefined || num1 == NaN){
         num1 = 0;
     } 
-    return num1-num2;
+    if (Number.isInteger(num1 - num2)){
+        return num1-num2;
+    } else {
+        return Number((num1-num2).toFixed(2));
+    }
+    
 }
 
 function multiply(num1,num2){
@@ -22,7 +32,12 @@ function multiply(num1,num2){
     } else if (num1 == undefined || num1 == NaN){
         num1 = 0;
     } 
-    return num1*num2;
+    if (Number.isInteger(num1 * num2)){
+        return num1*num2;
+    } else {
+        return Number((num1*num2).toFixed(2));
+    }
+    
 }
 
 function divide(num1,num2){
@@ -34,7 +49,12 @@ function divide(num1,num2){
     if (num2 == 0) {
         return 'ERROR';
     } else {
-        return num1/num2;
+        if (Number.isInteger(num1 / num2)){
+            return num1/num2;
+        } else {
+            return Number((num1/num2).toFixed(2));
+        }
+        
     }
     
 }
@@ -44,6 +64,7 @@ const display = document.querySelector('.display');
 const equal = document.querySelector('#equal');
 const backspace = document.querySelector('#backspace');
 const clear = document.querySelector('#clear');
+const decimal = document.querySelector('#decimal');
 
 display.textContent = '';
 display.setAttribute('operatorCounter',0);
@@ -133,18 +154,22 @@ equal.addEventListener('click', ()=>{
         num1 = Number(numDisp.split('+')[0]);
         num2 = Number(numDisp.split('+')[1]);
         display.textContent = add(num1,num2);
+        display.setAttribute('operatorCounter',0);
     } else if (numDisp.includes('—')){
         num1 = Number(numDisp.split('—')[0]);
         num2 = Number(numDisp.split('—')[1]);
         display.textContent = subtract(num1,num2);
+        display.setAttribute('operatorCounter',0);
     } else if (numDisp.includes('x')){
         num1 = Number(numDisp.split('x')[0]);
         num2 = Number(numDisp.split('x')[1]);
         display.textContent = multiply(num1,num2);
+        display.setAttribute('operatorCounter',0);
     } else if (numDisp.includes('/')){
         num1 = Number(numDisp.split('/')[0]);
         num2 = Number(numDisp.split('/')[1]);
         display.textContent = divide(num1,num2);
+        display.setAttribute('operatorCounter',0);
     } else {
         display.textContent = display.textContent;
     }
@@ -180,5 +205,45 @@ backspace.addEventListener('click', ()=>{
     
 })
 
+decimal.addEventListener('click', () => {
+    let numDisp = display.textContent;
+    let lastElement = numDisp[numDisp.length-1];
+    let secondNum;
+    if ('+'.includes(numDisp) ||
+        '—'.includes(numDisp) ||
+        'x'.includes(numDisp) ||
+        '/'.includes(numDisp)){
+        if (numDisp.lastIndexOf('+') != -1){
+            secondNum = numDisp.split('+')[1];
+            if (!(secondNum.includes('.'))){
+                display.textContent = display.textContent + '.';
+            }
 
+        } else if (numDisp.lastIndexOf('—') != -1){
+            secondNum = numDisp.split('—')[1];
+            if (!(secondNum.includes('.'))){
+                display.textContent = display.textContent + '.';
+            }
+
+        } else if (numDisp.lastIndexOf('x') != -1){
+            secondNum = numDisp.split('x')[1];
+            if (!(secondNum.includes('.'))){
+                display.textContent = display.textContent + '.';
+            }
+
+
+        } else {
+            secondNum = numDisp.split('/')[1];
+            if (!(secondNum.includes('.'))){
+                display.textContent = display.textContent + '.';
+            }
+
+        }
+    } else {
+        if ('1234567890'.includes(lastElement)){
+            display.textContent = display.textContent + '.';
+        }
+    }
+    
+})
 
